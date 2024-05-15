3a845a6d9956aaa12661942e1cf108c33dc47882
Habiba ahmed alaa eldin <101905453+Habibaaahmed@users.noreply.github.com>
2024-05-15 03:54:16 +0300

    Time: 0 ms (100.00%), Space: 10.2 MB (54.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-      
         if(digits[digits.size()-1]<9){
             digits[digits.size()-1]=digits[digits.size()-1]+1;
         }
         else{
-     for(int i=digits.size()-1;i>=0;i-- ){
+        for(int i=digits.size()-1;i>=0;i-- ){
          if(digits[i]==9 && i!=0){
              digits[i]=0;
 
@@ -15,7 +14,6 @@ public:
              digits.insert(digits.begin(),1);
          }else if(digits[i]!=9 ){
              digits[i]=digits[i]+1;
-             digits[i+1]=0;
              break;
              
          }
