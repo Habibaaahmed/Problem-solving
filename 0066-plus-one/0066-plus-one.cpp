dde3b2dfc49b31b142022127a32f0946c45f7477
Habiba ahmed alaa eldin <101905453+Habibaaahmed@users.noreply.github.com>
2024-05-15 03:55:43 +0300

    Time: 0 ms (100.00%), Space: 10.3 MB (54.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -8,16 +8,15 @@ public:
         for(int i=digits.size()-1;i>=0;i-- ){
          if(digits[i]==9 && i!=0){
              digits[i]=0;
-
         }else if(digits[i]==9 && i==0){
               digits[i]=0;
              digits.insert(digits.begin(),1);
          }else if(digits[i]!=9 ){
-             digits[i]=digits[i]+1;
-             break;
-             
+             digits[i]=digits[i]+1; 
+             break;    
+          }
          }
-     }}
+        }
         
         return digits;
     }
