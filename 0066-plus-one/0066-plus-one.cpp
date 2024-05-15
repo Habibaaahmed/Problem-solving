0d344f009f7f34db56b1345fd1026ef1e666da2f
Habiba ahmed alaa eldin <101905453+Habibaaahmed@users.noreply.github.com>
2024-05-15 03:53:08 +0300

    Time: 0 ms (100.00%), Space: 10.1 MB (99.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result=digits;
+      
         if(digits[digits.size()-1]<9){
-            result[digits.size()-1]=digits[digits.size()-1]+1;
+            digits[digits.size()-1]=digits[digits.size()-1]+1;
         }
         else{
-          //  result[digits.size()-1]=1;
-            //result.push_back(0);
      for(int i=digits.size()-1;i>=0;i-- ){
-         if(result[i]==9 && i!=0){
-             result[i]=0;
+         if(digits[i]==9 && i!=0){
+             digits[i]=0;
 
-        }else if(result[i]==9 && i==0){
-              result[i]=0;
-             result.insert(result.begin(),1);
-         }else if(result[i]!=9 ){
-             result[i]=digits[i]+1;
-             result[i+1]=0;
+        }else if(digits[i]==9 && i==0){
+              digits[i]=0;
+             digits.insert(digits.begin(),1);
+         }else if(digits[i]!=9 ){
+             digits[i]=digits[i]+1;
+             digits[i+1]=0;
              break;
              
          }
      }}
         
-        return result;
+        return digits;
     }
 };
