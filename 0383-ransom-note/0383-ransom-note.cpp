38687914758fe9c7cdc31d2f5e0d8ed4f063b141
Habiba ahmed alaa eldin <101905453+Habibaaahmed@users.noreply.github.com>
2024-06-04 16:50:40 +0300

    Time: 27 ms (8.99%), Space: 11.3 MB (34.56%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,18 +3,15 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> magazine_char;
         
-        for(auto i: magazine){
-         magazine_char[i]= magazine_char[i]+1;
-      
-        }
-         for(auto i: ransomNote){
+        for(auto i: magazine) magazine_char[i]= magazine_char[i]+1;
+         for(auto i: ransomNote)
             if ( magazine_char[i] !=0){
               magazine_char[i]= magazine_char[i]-1;   
             }
             else{
                 return false;
             }
-    }
+    
     return true;
     }
 };
